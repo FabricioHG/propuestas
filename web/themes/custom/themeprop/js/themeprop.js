9d2e737375b06ee0b053a62174ac414ad34a17ed
Fabricio Hernandez <fabriciofranciscohernandezgome@gmail.com>
2024-03-10 14:45:43 -0600

    Pagina propuestas v1 produccion

---
+++ web/themes/custom/themeprop/js/themeprop.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * themeprop behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.themeprop = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
